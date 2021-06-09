fb18a0ef57b4d32f11a6ea78453fd395c1b20183
Tequilian <kage94@gmx.de>
2021-06-09 10:04:37 +0200

    benchmarks

--- examples/merging/merge.cpp
+++ examples/merging/merge.cpp
@@ -11,21 +11,38 @@ public:
     Timer(){
         start = std::chrono::high_resolution_clock::now();
     }
+    
+    Timer(int size, int blocks, int threads){
+        sze = size;
+        blcks = blocks;
+        thrds = threads;
+        start = std::chrono::high_resolution_clock::now();
+    }
+
     ~Timer(){
         auto end = std::chrono::high_resolution_clock::now();
         auto dur = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
         double time = dur*0.000000001;
-        printf("%f s -> %f ns \n", time, (double)dur);
+        if(blcks == 0){
+            printf("%f s -> %f ns \n", time, (double)dur);
+        } else {
+            //time in ns (double)dur
+            printf("%i        %i        %i        %f        s\n", sze, blcks, thrds, time);
+        }
     }
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    int blcks = 0;
+    int sze = 0;
+    int thrds = 0;
 };
 
 
 
 int main() {
 
-    static constexpr size_t N = 1024;
+    static constexpr size_t N = 1000000;
+    static constexpr int thrds = 512;
 
     xpu::initialize(xpu::driver::cuda);
 
@@ -40,10 +57,14 @@ int main() {
 
     xpu::copy(a, xpu::host_to_device);
     xpu::copy(b, xpu::host_to_device);
-
+    for(int i = 1; i<=2048; i*=10)
     {
-        Timer timer;
-        xpu::run_kernel<GpuMerge>(xpu::grid::n_blocks(1), a.device(), a.size(), b.device(), b.size(), dst.device());
+        Timer timer(N, i,thrds);
+        xpu::run_kernel<GpuMerge>(xpu::grid::n_blocks(i), a.device(), a.size(), b.device(), b.size(), dst.device());
+    }
+    {
+        Timer timer(N, 2000,thrds);
+        xpu::run_kernel<GpuMerge>(xpu::grid::n_blocks(2000), a.device(), a.size(), b.device(), b.size(), dst.device());
     }
 
     xpu::copy(dst, xpu::device_to_host);
